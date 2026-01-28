634ba9adcd68ee664b3edde62310b0eb785924a9
ilzida bagautdinova <@DESKTOP-O6G1N04>
2026-01-28 15:11:13 +0500

    feat1 : created BankAccount base class

---
+++ BankApp/Models/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp.Models
+{
+    public abstract class BankAccount
+    {
+        public string AccountNumber { get; }
+        public string AccountName { get; }
+
+        protected BankAccount(string accountNumber, string accountName)
+        {
+            AccountNumber = accountNumber;
+            AccountName = accountName;
+        }
+        public abstract void DisplayInfo();
+    }
+}
