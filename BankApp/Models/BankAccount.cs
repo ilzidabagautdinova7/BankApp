850fa592440296c633926aae306f0166876542f8
ilzida bagautdinova <@DESKTOP-O6G1N04>
2026-01-28 15:33:33 +0500

    feat 2 : implemented encapsulation and validation

--- BankApp/Models/BankAccount.cs
+++ BankApp/Models/BankAccount.cs
@@ -6,14 +6,42 @@ namespace BankApp.Models
 {
     public abstract class BankAccount
     {
-        public string AccountNumber { get; }
-        public string AccountName { get; }
+        private decimal _balance;
+        public string AccountNumber { get; private set; }
+        public string AccountName { get; private set; }
 
-        protected BankAccount(string accountNumber, string accountName)
+        protected BankAccount(string accountNumber, string accountName , decimal initialBalance)
         {
-            AccountNumber = accountNumber;
+            if (string.IsNullOrWhiteSpace(accountNumber))
+                throw new ArgumentException("Номер счета не может быть пустым");
+            if (string.IsNullOrWhiteSpace(accountName))
+                throw new ArgumentException("Имя владельца не может быть пусты");
+            if (initialBalance < 0)
+                throw new ArgumentException("Начальный баланс не может быть отрицательным");
+
             AccountName = accountName;
+            AccountNumber = accountNumber;
+            _balance = initialBalance;
         }
+
+        public decimal Balance
+        {
+            get => _balance;
+            protected set
+            {
+                if (value < 0)
+                    throw new InvalidOperationException("Баланс не может быть отрицательным");
+                _balance = value;
+            }
+        }
+
+        public virtual void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+                throw new InvalidOperationException("Сумма пополнения дллжна быть положителбной");
+            Balance += amount;
+        }
+        public abstract void Withdraw(decimal amount);
         public abstract void DisplayInfo();
     }
 }
